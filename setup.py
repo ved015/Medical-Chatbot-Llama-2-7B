17dad80eb8654e9e53314fc35d9f1663cd243e34
Vedant Mahajan <vedantmahajan@Vedants-MacBook-Pro.local>
2024-03-26 18:25:10 +0530

    final

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Vedant Mahajan',
+    author_email= 'youremail@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
